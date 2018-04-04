2a3406ae0618407c613dbb85ea11c5e97d574fe8
irawanwimo <irawandwihastomo@ymail.com>
2018-04-04 18:13:52 +0700

    Fix

--- api/routes/dishRouter.js
+++ api/routes/dishRouter.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const router = express.Router();
 const mongoose = require("mongoose");
+const router = express.Router();
 
 const Dishes = require("../../model/dish");
 
--- app.js
+++ app.js
@@ -1,8 +1,25 @@
 const express = require('express');
 const app = express();
+const bodyParser = require("body-parser");
 const mongoose = require('mongoose');
 
 mongoose.connect('mongodb+srv://storeking:storeking@storeking-pac3w.mongodb.net/test');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+    if (req.method === "OPTIONS") {
+        res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+        return res.status(200).json({});
+    }
+    next();
+});
 
 const dishRouter = require('./api/routes/dishRouter');
 const promoRouter = require('./api/routes/promoRouter');
