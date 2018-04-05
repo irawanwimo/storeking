86b0aac113f00a2c842b4752714deb0cdda612bc
irawanwimo <irawandwihastomo@ymail.com>
2018-04-05 11:50:07 +0700

    Add working CRUD for dishes

--- api/routes/dishRouter.js
+++ api/routes/dishRouter.js
@@ -4,9 +4,24 @@ const router = express.Router();
 
 const Dishes = require("../../model/dish");
 
-router.get('/:id', (req, res, next) => {
-    res.status(200).json({
-        message: 'Handling GET requests to /dishes'
+router.get("/", (req, res, next) => {
+    Dishes.find()
+    .exec()
+    .then(docs => {
+    console.log(docs);
+    //   if (docs.length >= 0) {
+    res.status(200).json(docs);
+    //   } else {
+    //       res.status(404).json({
+    //           message: 'No entries found'
+    //       });
+    //   }
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({
+            error: err
+        });
     });
 });
 
@@ -33,10 +48,60 @@ router.post('/', (req, res, next) => {
     });
 });
 
-router.delete('/:id', (req, res, next) => {
-    res.status(200).json({
-        message: 'Handling GET requests to /dishes'
-    });
+router.get("/:personId", (req, res, next) => {
+    const id = req.params.personId;
+    Dishes.findById(id)
+      .exec()
+      .then(doc => {
+        console.log("From database", doc);
+        if (doc) {
+          res.status(200).json(doc);
+        } else {
+          res
+            .status(404)
+            .json({ message: "No valid entry found for provided ID" });
+        }
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json({ error: err });
+      });
+});
+
+router.patch("/:personId", (req, res, next) => {
+    const id = req.params.personId;
+    const updateOps = {};
+    for (const ops of req.body) {
+      updateOps[ops.propName] = ops.value;
+    }
+    Product.update({ _id: id }, { $set: updateOps })
+      .exec()
+      .then(result => {
+        console.log(result);
+        res.status(200).json(result);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json({
+          error: err
+        });
+      });
+});
+  
+
+  router.delete("/:personId", (req, res, next) => {
+    const id = req.params.personId;
+    Dishes.remove({ _id: id })
+      .exec()
+      .then(result => {
+        res.status(200).json(result);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json({
+          error: err
+        });
+      });
 });
 
 module.exports = router;
--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 const bodyParser = require("body-parser");
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://storeking:storeking@storeking-pac3w.mongodb.net/test');
+mongoose.connect('mongodb://storeking:storeking@storeking-shard-00-00-pac3w.mongodb.net:27017,storeking-shard-00-01-pac3w.mongodb.net:27017,storeking-shard-00-02-pac3w.mongodb.net:27017/test?ssl=true&replicaSet=storeking-shard-0&authSource=admin');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -25,10 +26,10 @@ const dishRouter = require('./api/routes/dishRouter');
 const promoRouter = require('./api/routes/promoRouter');
 const leaderRouter = require('./api/routes/leaderRouter');
 
+app.use(morgan('dev'));
+
 app.use('/dishes', dishRouter);
-
 app.use('/promotions', promoRouter);
-
 app.use('/leadership', leaderRouter);
 
 module.exports = app;
